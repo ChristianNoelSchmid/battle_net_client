08464bb81ee27fd763524f34348ca603ea275d8f
Christian Schmid <christian.noel.schmid@gmail.com>
2024-08-18 22:13:58 -0400

    Added entrance to battle scene, and choice menu for battle / riddle quest in main menu.

--- config.gd
+++ config.gd
@@ -5,3 +5,6 @@ const URL_ROOT = "http://127.0.0.1:3005"
 const CARD_CONSIDERED = 0
 const CARD_NOT_CONSIDERED = 1
 const CARD_CONFIRMED = 2
+
+const BATTLE_QUEST_IDX = 0
+const RIDDLE_QUEST_IDX = 1
--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -4,14 +4,17 @@ const ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJleHBpcmVzIjoiMjAyNC0wOC0xOVQxOToxN
 
 var _game_state_http_request: HTTPRequest
 var _quest_state_http_request: HTTPRequest
-var _start_monster_quest_request: HTTPRequest
-var _start_riddle_quest_request: HTTPRequest
+var _start_quest_request: HTTPRequest
 
+var _quest_type_panel: Control 
 var _loading_panel: Control
 
 func _ready():
+	_quest_type_panel = get_node("/root/Main/Background/QuestTypePanel")
+	_quest_type_panel.hide()
+
 	_loading_panel = get_node("/root/Main/LoadingPanel")
-	_loading_panel.visible = true
+	_loading_panel.show()
 
 	_game_state_http_request = HTTPRequest.new()
 	_game_state_http_request.request_completed.connect(_on_game_state_response)
@@ -21,9 +24,9 @@ func _ready():
 	_quest_state_http_request.request_completed.connect(_on_quest_state_response)
 	add_child(_quest_state_http_request)
 
-	_start_monster_quest_request = HTTPRequest.new()
-	_start_monster_quest_request.request_completed.connect(_on_start_monster_quest_response)
-	add_child(_start_monster_quest_request)
+	_start_quest_request = HTTPRequest.new()
+	_start_quest_request.request_completed.connect(_on_start_quest_response)
+	add_child(_start_quest_request)
 
 	_game_state_http_request.request("%s/game/state" % Config.URL_ROOT,
 		["Authorization: Bearer %s" % ACCESS_TOKEN, "Content-Type: application/json"]
@@ -32,14 +35,33 @@ func _ready():
 		["Authorization: Bearer %s" % ACCESS_TOKEN, "Content-Type: application/json"]
 	)
 
+# Signal Handlers
 func on_quest_btn_pressed():
 	if GameState.quest_state == null:
-		_start_monster_quest_request.request("%s/quest/create/0" % Config.URL_ROOT,
+		_quest_type_panel.show()
+	else:
+		_loading_panel.visible = true
+		get_tree().change_scene_to_file("res://scenes/battle_scene/battle_scene.tscn")
+
+func on_battle_btn_pressed():
+	if GameState.quest_state == null:
+		_start_quest_request.request(
+			"%s/quest/create/%d" % [Config.URL_ROOT, Config.BATTLE_QUEST_IDX],
 			["Authorization: Bearer %s" % ACCESS_TOKEN, "Content-Type: application/json"],
 			HTTPClient.METHOD_POST,
 		)
-		_loading_panel.visible = true	
+		_loading_panel.visible = true
 
+func on_riddle_btn_pressed():
+	if GameState.quest_state == null:
+		_start_quest_request.request(
+			"%s/quest/create/%d" % [Config.URL_ROOT, Config.RIDDLE_QUEST_IDX],
+			["Authorization: Bearer %s" % ACCESS_TOKEN, "Content-Type: application/json"],
+			HTTPClient.METHOD_POST,
+		)
+		_loading_panel.visible = true
+
+# HTTP Response Handlers
 func _on_game_state_response(_result, _response_code, _headers, _body):
 	GameState.game_state = JSON.parse_string(_body.get_string_from_utf8())
 	print("Game State: %s" % GameState.game_state)
@@ -50,7 +72,7 @@ func _on_quest_state_response(_result, _response_code, _headers, _body):
 	print("Quest State: %s" % GameState.quest_state)
 	_loading_panel.visible = false
 
-func _on_start_monster_quest_response(_result, _response_code, _headers, _body):
+func _on_start_quest_response(_result, _response_code, _headers, _body):
 	GameState.quest_state = JSON.parse_string(_body.get_string_from_utf8())
-	print("Monster Quest Started! %s" % GameState.quest_state)
-	_loading_panel.visible = false
+	print("Quest Started! %s" % GameState.quest_state)
+	get_tree().change_scene_to_file("res://scenes/battle_scene/battle_scene.tscn")
